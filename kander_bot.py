cf66705507835f0338289299acf520b53f019949
yasharil <yasharil@rambler.ru>
2018-09-05 11:50:56 +0500

    Update kander_bot.py

--- kander_bot.py
+++ kander_bot.py
@@ -6,7 +6,7 @@ def startBot(bot, update):
     bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
 
 
-TOKEN = '674274976:AAFpC8eZMhx9KwFGE0i8gfRkEFMbpcyPVjE'
+TOKEN = <token>
 PORT = int(os.environ.get('PORT', '8443'))
 updater = Updater(TOKEN)
 dispatcher = updater.dispatcher
@@ -16,5 +16,5 @@ dispatcher.add_handler(start_handler)
 updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
                       url_path=TOKEN)
-updater.bot.set_webhook("https://quiet-stream-94228.herokuapp.com/" + TOKEN)
+updater.bot.set_webhook("https://<app>.herokuapp.com/" + TOKEN)
 updater.idle()
