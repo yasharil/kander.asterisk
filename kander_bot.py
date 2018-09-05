39d7cf098a51d67aa6bc1fc092b16118147664a2
yasharil <yasharil@rambler.ru>
2018-09-05 11:22:50 +0500

    Третий коммит

--- kander_bot.py
+++ kander_bot.py
@@ -1,10 +1,18 @@
 import os
 from telegram.ext import Updater, CommandHandler
 
+
+def startBot(bot, update):
+    bot.send_message(chat_id=update.message.chat_id, text="I'm a bot, please talk to me!")
+
+
 TOKEN = '674274976:AAFpC8eZMhx9KwFGE0i8gfRkEFMbpcyPVjE'
 PORT = int(os.environ.get('PORT', '8443'))
 updater = Updater(TOKEN)
-# add handlers
+dispatcher = updater.dispatcher
+start_handler = CommandHandler('start', startBot)
+dispatcher.add_handler(start_handler)
+
 updater.start_webhook(listen="0.0.0.0",
                       port=PORT,
                       url_path=TOKEN)
