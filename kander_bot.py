f8ddc5dd86c15de774d2d1d17b289bb8b816fedf
Ilia Sharnin <yasharil@tmn-aho-01.tjm.yamal-airlines.ru>
2018-09-05 10:43:02 +0500

    Первый коммит

---
+++ kander_bot.py
@@ -0,0 +1,12 @@
+import os
+from telegram.ext import Updater, CommandHandler
+
+TOKEN = '674274976:AAFpC8eZMhx9KwFGE0i8gfRkEFMbpcyPVjE'
+PORT = int(os.environ.get('PORT', '8443'))
+updater = Updater(TOKEN)
+# add handlers
+updater.start_webhook(listen="0.0.0.0",
+                      port=PORT,
+                      url_path=TOKEN)
+updater.bot.set_webhook("https://<appname>.herokuapp.com/" + TOKEN)
+updater.idle()
